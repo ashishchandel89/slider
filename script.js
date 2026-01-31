c4734835a77cedf39e9b1677d799a0d96510b828
ashishchandel89 <chandel078ashish@gmail.com>
2026-01-31 18:43:18 +0530

    last updates

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 
-//Some Javascript Basics.
+// //Some Javascript Basics.
 
-let a = Number(prompt("Enter a no:"));
-console.log("Square of a: "+(a*a));
-console.log("Cube of a: "+(a*a*a));
-let fact=1;
-for(let i=a;i>=1;i--){
-    fact=fact*i;
-}
-console.log("Factorial of a: "+(fact));
+// let a = Number(prompt("Enter a no:"));
+// console.log("Square of a: "+(a*a));
+// console.log("Cube of a: "+(a*a*a));
+// let fact=1;
+// for(let i=a;i>=1;i--){
+//     fact=fact*i;
+// }
+// console.log("Factorial of a: "+(fact));
 
 
-console.table({Name: "Ashish", Email: "chandel078ashish@gmail.com", Phone: "8221876090"})
+// console.table({Name: "Ashish", Email: "chandel078ashish@gmail.com", Phone: "8221876090"})
 
-let s1="AshishChandel"
-console.log(s1.split(''));
-console.log(s1.replace("Chandel","Thakur"));
-console.log(s1.slice(2,7));
-console.log(s1.includes("d"));
-console.error(s1+" may contain error");
-console.warn( "Warning: This is not supported by this langauge");
-console.info("Server started successfully");        // Not more differnce in info and log
-console.group("User Information");              // Helps groups multiple logs together 
-console.log(s1);
-console.log("class: Btech 3rd yr");
-console.groupEnd();
+// let s1="AshishChandel"
+// console.log(s1.split(''));
+// console.log(s1.replace("Chandel","Thakur"));
+// console.log(s1.slice(2,7));
+// console.log(s1.includes("d"));
+// console.error(s1+" may contain error");
+// console.warn( "Warning: This is not supported by this langauge");
+// console.info("Server started successfully");        // Not more differnce in info and log
+// console.group("User Information");              // Helps groups multiple logs together 
+// console.log(s1);
+// console.log("class: Btech 3rd yr");
+// console.groupEnd();
 
-console.time("looptime");               // It display the timing of execution of loop
-for(let i=a;i>=1;i--){
-    fact=fact*i;
-}
-console.timeEnd("looptime");            // this is mandatory "looptime"
-console.log("Factorial of a: "+(fact));
-//console.clear();
-let s2=`Ashish has got ${Math.round(7.9+7.3+7.43+9.04)/4} cgpa till four semester`;
-console.log(s2);
+// console.time("looptime");               // It display the timing of execution of loop
+// for(let i=a;i>=1;i--){
+//     fact=fact*i;
+// }
+// console.timeEnd("looptime");            // this is mandatory "looptime"
+// console.log("Factorial of a: "+(fact));
+// //console.clear();
+// let s2=`Ashish has got ${Math.round(7.9+7.3+7.43+9.04)/4} cgpa till four semester`;
+// console.log(s2);
+
+
+
+console.log("Ashish"*2);            //NaN
+
+let arr1=[1,2,3,4];                 // Reference array
+let arr2=arr1;
+arr2.pop();
+console.log(arr1);
