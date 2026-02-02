a5cae13a47680a52c1d9ed3a84302f8136def0db
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-02 21:17:52 +0530

    new update on 02-022026

--- script.js
+++ script.js
@@ -38,9 +38,95 @@
 
 
 
-console.log("Ashish"*2);            //NaN
+// console.log("Ashish"*2);            //NaN
 
-let arr1=[1,2,3,4];                 // Reference array
-let arr2=arr1;
-arr2.pop();
-console.log(arr1);
+// let arr1=[1,2,3,4];                 // Reference array
+// let arr2=arr1;
+// arr2.pop();
+// console.log(arr1);
+
+// console.log(1/0);
+// console.log(-1/0);
+
+// let isCodingfun=true;
+// let isEqual=true;
+// console.log(isCodingfun == isEqual);
+
+// let name="Ritik";
+// let s1="Hello";
+// console.log(`Welcome ${name} `+s1);
+
+
+// console.log("Connected Successfully")
+
+// {
+//     let a=12;
+//     var b=13;
+// }
+
+// console.log(b);
+
+// let name=prompt("Enter Your Name: ");
+// alert(`Hello ${name}`);
+
+// let age1=prompt("Enter your age: ");
+// console.log(age1+'5');
+
+// let num1=prompt("Enter a num1: ");
+// let num2=prompt("Enter a num2: ");
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1%num2);
+// console.log(num1**num2);
+
+
+// let x=5;
+// x=x+3;
+// x+=3;
+// x-=4;
+
+// console.log(5=='5');
+// console.log(5==='5');
+// console.log(5 & 4);
+
+
+// test();
+// function test(){
+//     let s1="hello";
+//     console.log(`Say,"${s1}"`);
+// }
+
+// //Hoisted is known as declaration of any variable in var is hoisted. but in the concept of const and let after initialization a variable does not a hoisted
+
+//  let age=prompt("Enter your age: ");
+//  let country=prompt("Enter your country");
+//  console.log(Number(age));
+//  console.log(country);
+//  age>=18 && country==='india' ? console.log("You are eligible for vote") : console.log("you are not eligible for vote")
+// // age>=18 ? console.log("You are Eligible for vote.") : console.log("You are not eligible for vote");
+
+// let city=prompt("Enter a city");
+// city==='bhopal' ? console.log("Mp") : city==='delhi' ?  console.log("capital") : console.log("Unknown")
+
+// let score=prompt("Enter your score to check you are pass or fail");
+// score>=40 ? console.log("pass") : console.log("fail")
+
+// let day=prompt("Enter a day");
+// switch(day){
+// case '1':
+//     console.log("Sunday");
+//      break;
+// case '2':
+//     console.log("Monday");
+//      break;
+// case '3':
+//      console.log("Tuesday");
+// break;
+
+// default:
+// console.log("Invalid");
+// }
+let s1="hello ashish chandel how are you?";
+console.log(s1.split());
